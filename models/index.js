332588207064d8c077c9229a46048d43c0924f64
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-09-19 23:41:25 -0700

    Removed deprecated lines in DB connection

--- models/index.js
+++ models/index.js
@@ -3,10 +3,8 @@ const DB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/airQuality'
 
 mongoose.connect(DB_URI, {
   useNewUrlParser: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
   useUnifiedTopology: true,
-
+  useCreateIndex: true
 })
   .then(() => console.log('MongoDB connected successfully'))
   .catch((err) => console.log(err));
