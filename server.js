20ff336ef5e92d46a2630a77d7a3ae0cbe775bcd
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-09-19 17:44:13 -0700

    Created update method that will create a db instance if doesn't exist and api for showing all db records

--- server.js
+++ server.js
@@ -3,28 +3,44 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const https = require('https');
+const db = require('./models');
 require('dotenv').config();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
-app.get('*', (req, res) => {
+app.get('/api/v1/update', (req, res) => {
   const options = {
     hostname: 'www.airnowapi.org',
-    path: `/aq/observation/zipCode/current/?format=application/json&zipCode=94080&distance=25&API_KEY=${API_KEY}`,
+    path: `/aq/observation/zipCode/current/?format=application/json&zipCode=94080&distance=25&API_KEY=${process.env.API_KEY}`,
     method: 'GET'
   };
 
   const request = https.request(options, resp => {
-    let data = "";
+    let dataStr = "";
+    let newDBRecord = {};
+
 
     resp.on('data', d => {
-      data += d;
+      dataStr += d;
     });
 
+    //create db instance and send status
     resp.on('end', () => {
-      res.send(data)
+      let dataArr = JSON.parse(dataStr);
+      newDBRecord.ReportingArea = dataArr[0].ReportingArea;
+      newDBRecord.ParticulateMatter = dataArr[1].AQI;
+      newDBRecord.Ozone = dataArr[0].AQI;
+
+      db.AirQuality.updateOne({ ReportingArea: newDBRecord.ReportingArea }, newDBRecord, { upsert: true }, (err, updatedRecord) => {
+
+        if (err) {
+          return console.log(err);
+        }
+
+        res.status(200).json({ status: 200, message: 'instance created', record: updatedRecord })
+      })
     });
   });
 
@@ -34,5 +50,16 @@ app.get('*', (req, res) => {
 
   request.end();
 });
+
+
+app.get('/api/v1/dbRecords', (req, res) => {
+  db.AirQuality.find({}, (err, foundRecords) => {
+    if (err) {
+      return console.log(err);
+    }
+
+    res.send(foundRecords);
+  })
+})
 
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}/`));
