4f343e2b550fb7f1a1cdba24479951b022700048
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-09-19 17:43:01 -0700

    Fixed style

--- models/AirQualityModel.js
+++ models/AirQualityModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const AirQualitySchema = new mongoose.Schema({
   ReportingArea: String,
   ParticulateMatter: Number,
-  O3: Number,
+  Ozone: Number,
 }, { timestamps: true });
 
 module.exports = mongoose.model('AirQuality', AirQualitySchema);
