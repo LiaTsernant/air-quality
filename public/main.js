cdf15c814a740024bb5ef86443dee058d6cdf94a
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-09-20 21:31:58 -0700

    Added 6 particles files for different statuses

--- public/main.js
+++ public/main.js
@@ -1,3 +1,4 @@
+// Color palettes
 let GOOD_COLORS = [
   'rgba(127, 255, 212, 0.8)',
   'rgba(47, 221, 163, 0.7)',
@@ -41,15 +42,19 @@ fetch('/api/v1/southSanFranciscoRecord').
   );
 
 function renderPage(dataObj) {
+  // Set PM2.5 value
   let particulateMatter = document.getElementById('particulate-matter-data');
   particulateMatter.textContent = dataObj.ParticulateMatter;
 
+  // Set ozone value
   let ozoneData = document.getElementById('ozone-data');
   ozoneData.textContent = dataObj.Ozone;
 
+  // Set area name
   let area = document.getElementById('area');
   area.textContent = dataObj.ReportingArea;
 
+  // Set air quality status
   setStatus(dataObj.ParticulateMatter)
 }
 
@@ -58,29 +63,35 @@ function setStatus(particulateMatter) {
   let colorPalette;
 
   if (particulateMatter <= 12.0) {
+    loadParticles('good_particles.json');
     statusData = 'good';
     colorPalette = GOOD_COLORS;
   } else if (particulateMatter > 12.0 && particulateMatter <= 35.4) {
+    loadParticles('moderate_particles.json');
     statusData = 'moderate';
     colorPalette = MODERATE_COLORS;
   } else if (particulateMatter > 35 && particulateMatter <= 55.4) {
+    loadParticles('bad_particles.json');
     statusData = 'bad';
     colorPalette = BAD_COLORS;
   } else if (particulateMatter > 55.4 && particulateMatter <= 150.4) {
+    loadParticles('unhealthy_particles.json');
     statusData = 'unhealthy';
     colorPalette = UNHEALTHY_COLORS;
   } else if (particulateMatter > 150.4 && particulateMatter <= 250.4) {
+    loadParticles('dangerous_particles.json');
     statusData = 'dangerous';
     colorPalette = DANGEROUS_COLORS;
   } else if (particulateMatter > 250.5) {
+    loadParticles('hazardous_particles.json');
     statusData = 'hazardous';
     colorPalette = HAZARDOUS_COLORS;
-  }
-
+  };
+  
   let status = document.getElementById('status');
   status.textContent = statusData;
 
-
+  // Pick palette for the page
   setColorPalette(colorPalette);
 }
 
@@ -92,6 +103,11 @@ function setColorPalette(palette) {
   let circles = [circle, circle2, circle3, circle4];
   circles.forEach((circle, i) => circle.setAttribute('style', `background-color: ${palette[i]}`));
   let area = document.getElementById('area');
-
+  // Change color of area to match palette
   area.setAttribute('style', `color: ${palette[2]}`)
 }
+
+function loadParticles(JSONparticleToLoad) {
+  // Loades particles(dust json file)
+  particlesJS.load('particles-js', `particles/${JSONparticleToLoad}`)
+};
